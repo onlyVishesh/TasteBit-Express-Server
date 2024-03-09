138694e2fe0a5b5e0b5c66aff6d704562908ca2d
onlyVishesh <visheshmaurya14@gmail.com>
2024-03-09 13:52:51 +0530

    Server is working

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/api/menu', async (req, res) => {
 
 
 app.get('/', (req, res) => {
-    res.json({ "test": "Welcome to FoodFire! - See Live Web URL for this Server - https://foodfire-app.netlify.app" });
+    res.json({ "test": "Welcome to TasteBit Express! - See Live Web URL for this Server - " });
 })
 
 app.listen(port, () => {
